e2fc984e754081758c6ab6658152fb27cf8c433f
Max <zeetreex@gmail.com>
2023-09-01 19:54:43 +0300

    signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -40,7 +40,31 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return 'I am signed in';
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    delete user.hash;
+
+    return user;
   }
 }
