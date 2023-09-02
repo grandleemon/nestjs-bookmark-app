f884005af51dd004e681f0d1998b426bb6ce6a69
Max <zeetreex@gmail.com>
2023-09-02 13:22:49 +0300

    auth guard validating access token -> custom guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return req.user;
+  }
+}
